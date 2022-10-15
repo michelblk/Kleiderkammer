e43673c70841d3144228deda2e10a8646067c751
Michel Blank <michelblk@users.noreply.github.com>
2022-10-15 11:06:52 +0200

    add waeschen

---
+++ kleiderkammer/kleidung/model/kleidungswaesche.py
@@ -0,0 +1,10 @@
+from kleiderkammer.util.db import db
+
+
+class Kleidungswaesche(db.Model):
+    __tablename__ = 'kleidungswaesche'
+    id = db.Column(db.BigInteger, primary_key=True)
+    kleidung_id = db.Column(db.BigInteger, db.ForeignKey('kleidung.id'))
+    kleidung = db.relationship("Kleidung", backref=db.backref("kleidung", uselist=False))
+    von = db.Column(db.DateTime, nullable=False)
+    bis = db.Column(db.DateTime, nullable=True)
