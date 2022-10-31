bd83969b1425f246f606966633722506d804100c
Michel Blank <michelblk@users.noreply.github.com>
2022-10-31 20:31:26 +0100

    Zeige Anzahl aktuell ausgeliehener Kleidung

--- kleiderkammer/mitglieder/views.py
+++ kleiderkammer/mitglieder/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from sqlalchemy.sql.functions import count, coalesce
 
 from kleiderkammer.kleidung.model.kleidungsleihe import Kleidungsleihe
 from kleiderkammer.mitglieder.model.mitglied import Mitglied
@@ -10,12 +11,22 @@ mitglieder = Blueprint('mitglieder', __name__, template_folder="templates")
 @mitglieder.route("/")
 @oidc.require_login
 def index():
+    ausgeliehene_kleidung = Kleidungsleihe.query \
+        .with_entities(Kleidungsleihe.mitglied_id, count(Kleidungsleihe.kleidung_id).label("count")) \
+        .filter(Kleidungsleihe.bis == None) \
+        .subquery()
+
     rows = Mitglied.query \
         .filter_by(aktiv=True) \
+        .join(Kleidungsleihe, Mitglied.id == Kleidungsleihe.mitglied_id, isouter=True) \
+        .join(ausgeliehene_kleidung, Mitglied.id == ausgeliehene_kleidung.c.mitglied_id, isouter=True) \
+        .with_entities(Mitglied.id, Mitglied.nachname, Mitglied.vorname,
+                       # Anzahl der aktuell ausgeliehene Kleidung
+                       coalesce(ausgeliehene_kleidung.c.count, 0).label("ausgeliehene_kleidung_count")
+                       ) \
+        .group_by(Mitglied) \
         .order_by(Mitglied.nachname, Mitglied.vorname) \
         .all()
-
-    Mitglied.query.join(Kleidungsleihe).all()  # FIXME
 
     return render_template("html/mitglieder.html", rows=rows)
 
