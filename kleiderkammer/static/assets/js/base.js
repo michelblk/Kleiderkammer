eed4554fc875189838177e42ce343f2791e415b2
Michel Blank <michelblk@users.noreply.github.com>
2023-02-26 12:03:18 +0100

    format_date-Helper robuster machen

--- kleiderkammer/static/assets/js/base.js
+++ kleiderkammer/static/assets/js/base.js
@@ -1,5 +1,8 @@
 "use strict";
 
 Handlebars.registerHelper("format_date", function (dateString, format) {
-    return $.format.date(dateString, format);
+    if (dateString && format) {
+        return $.format.date(dateString, format);
+    }
+    return undefined;
 });
